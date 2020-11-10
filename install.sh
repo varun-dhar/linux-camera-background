c3dfbd3a351a82927fea7797ed428f39e7815816
doggo42 <38188031+doggo4242@users.noreply.github.com>
2020-11-09 20:13:15 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt install v4l2loopback-dkms
+echo "options v4l2loopback devices=1 video_nr=20 card_label="bgcam" exclusive_caps=1" > /etc/modprobe.d/bgcam.conf
+echo v4l2loopback > /etc/modules-load.d/bgcam.conf
+modprobe -r v4l2loopback
+modprobe v4l2loopback
+python3 -m pip install numpy opencv-contrib-python pyfakewebcam
+chmod +x bg.py
