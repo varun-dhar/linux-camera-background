4162fb85237730ac69fe3b6741ed692df834ae66
doggo42 <38188031+doggo4242@users.noreply.github.com>
2020-12-19 14:32:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt install v4l2loopback-dkms
-echo "options v4l2loopback devices=1 video_nr=20 card_label="bgcam" exclusive_caps=1" > /etc/modprobe.d/bgcam.conf
+echo "options v4l2loopback devices=1 video_nr=20 card_label=bgcam exclusive_caps=1" > /etc/modprobe.d/bgcam.conf
 echo v4l2loopback > /etc/modules-load.d/bgcam.conf
 modprobe -r v4l2loopback
 modprobe v4l2loopback
