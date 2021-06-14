277935e098dbae289e4c0015da5576ddde2e5d45
doggo42 <38188031+doggo4242@users.noreply.github.com>
2021-06-14 19:47:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt install v4l2loopback-dkms
+apt install libopencv-dev v4l2loopback-dkms
 echo "options v4l2loopback devices=1 video_nr=20 card_label="bgcam" exclusive_caps=1" > /etc/modprobe.d/bgcam.conf
 echo v4l2loopback > /etc/modules-load.d/bgcam.conf
 modprobe -r v4l2loopback
