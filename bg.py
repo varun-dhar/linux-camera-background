c7314f4c6af518490b50f43ecbb24706450742d1
doggo42 <38188031+doggo4242@users.noreply.github.com>
2021-06-14 18:51:29 -0400

    Update bg.py

--- bg.py
+++ bg.py
@@ -31,37 +31,50 @@ from tf_bodypix.api import download_model, load_model, BodyPixModelPaths
 
 model = load_model(download_model(BodyPixModelPaths.MOBILENET_FLOAT_50_STRIDE_16))
 
-ops = []
-bgimage = None
-cam = wc.FakeWebcam('/dev/video20',1280,720)
 conf = configparser.ConfigParser()
 conf.read('options.txt')
-ops.append(int(conf['ADJUSTMENTS']['blurFactor']))
-ops.append(int(conf['ADJUSTMENTS']['useImageBg']))
-bgimage = conf['ADJUSTMENTS']['backgroundFile']
-ops.append(int(conf['ADJUSTMENTS']['maskSensitivity']))
-del conf
-ops[0] = ops[0]+1 if (ops[0]%2)==0 else ops[0]
+ops = dict(conf.items('ADJUSTMENTS'))
+ops = dict(map(lambda entry: (entry[0],int(entry[1]) if entry[1].isdigit() else entry[1]),ops.items()))
+ops['blurfactor'] = ops['blurfactor']+1 if (ops['blurfactor']%2)==0 else ops['blurfactor']
 
 cap = cv2.VideoCapture(0)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-while True:
-	ret,frame = cap.read()
-	if not ret:
-		break
-	mask = model.predict_single(frame)
-	mask = mask.get_mask(threshold=ops[2]/100)
-	inv_mask = 1-mask
-	clear = frame.copy()
-	if ops[1] == 0:
-		frame = cv2.blur(frame,(ops[0],ops[0]))
-	else:
-		frame = cv2.imread(bgimage)
-		frame = cv2.resize(frame,(width,height))
-	for i in range(frame.shape[2]):
-		clear[:,:,i] = clear[:,:,i]*mask[:,:,0] + frame[:,:,i]*inv_mask[:,:,0]
-	clear = cv2.cvtColor(clear,cv2.COLOR_BGR2RGB)
-	cam.schedule_frame(clear)
+if ops.get('framewidth') and ops['framewidth'] != width:
+	width = ops['framewidth']
+	cap.set(cv2.CAP_PROP_FRAME_WIDTH,width)
+
+if ops.get('frameheight') and ops['frameheight'] != height:
+	height = ops['frameheight']
+	cap.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
+
+bgimage = ops.get('backgroundfile')
+if bgimage:
+	bgimage = cv2.imread(bgimage)
+	bgimage = cv2.resize(bgimage,(width,height))
+
+cam = wc.FakeWebcam('/dev/video20',width,height)
+
+try:
+	while True:
+		ret,frame = cap.read()
+		if not ret:
+			break
+		mask = model.predict_single(frame)
+		mask = mask.get_mask(threshold=ops['masksensitivity']/100)
+		inv_mask = 1-mask
+		clear = frame.copy()
+		if not bgimage:
+			frame = cv2.blur(frame,(ops['blurfactor'],ops['blurfactor']))
+		else:
+			frame = bgimage
+
+		for i in range(frame.shape[2]):
+			clear[:,:,i] = clear[:,:,i]*mask[:,:,0] + frame[:,:,i]*inv_mask[:,:,0]
+		clear = cv2.cvtColor(clear,cv2.COLOR_BGR2RGB)
+		cam.schedule_frame(clear)
+except KeyboardInterrupt:
+	pass
+
 cap.release()
